64c411e8b032761ab58533b5ac244bafadc4033d
Ali-Ashraf-Abir <fazilabir88@gmail.com>
2023-03-05 00:34:23 +0600

    final commit

--- script.js
+++ script.js
@@ -132,7 +132,6 @@ function fetchSurahInfos(){
         const surahAyah=document.getElementById("surah-ayah")
 
             document.getElementById("english-translation").addEventListener('click',function(){
-                if(typeof(currentlyDisplaying)!="string"){
                 surahAyah.innerHTML=" "
                 if(typeof(currentlyDisplaying)!='string'){
                 for(let ayah of data.surahs[currentlyDisplaying].ayahs){
@@ -140,7 +139,7 @@ function fetchSurahInfos(){
                     <p class="bg-gray-900 text-white lg:text-2xl text-xl font-semibold mt-4 english py-2">${ayah.numberInSurah}. ${ayah.text}</p>
                     `
                 }
-            }}
+            }
                 })
     
     
@@ -154,8 +153,9 @@ function fetchSurahInfos(){
         const surahAyah=document.getElementById("surah-ayah")
 
         document.getElementById("bangla-translation").addEventListener('click',function(){
-            surahAyah.innerHTML=" "
+            
             if(typeof(currentlyDisplaying)!='string'){
+                surahAyah.innerHTML=" "
             for(let ayah of data[currentlyDisplaying].verses){
                 surahAyah.innerHTML+=`
                 <p class="bg-gray-900 text-white lg:text-2xl text-xl font-semibold mt-4 py-2">${ayah.id}. ${ayah.text}</p>
